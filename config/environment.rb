919cb6e290abab79346e9a3c459938f8c9ee6c12
Your Name <you@example.com>
2017-10-03 16:55:04 +0300

    555

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,5 @@
 require_relative 'application'
 
 # Initialize the Rails application.
+ActiveModel::SecurePassword.min_cost = true
 Rails.application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   get 'static_pages/index'
   get "static_pages/home"
-  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
     get 'static_pages/contact'
