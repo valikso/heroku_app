5379ff29fb3c5cd444b63976442cfebe886591bb
Your Name <you@example.com>
2017-10-03 23:08:04 +0300

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,10 +12,7 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
+  
   end
 
   describe "Help page" do
