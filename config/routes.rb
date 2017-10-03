65a56af70011f9e1a594d7d5b6abb4ad3c16dbc4
Your Name <you@example.com>
2017-10-03 21:52:25 +0300

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'static_pages/index'
-  #get "static_pages/home"
+  get "static_pages/home"
   root 'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  get 'static_pages/help'
+  get 'static_pages/about'
+    get 'static_pages/contact'
 end
