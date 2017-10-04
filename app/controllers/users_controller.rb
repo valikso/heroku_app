3b0eab63f0b3d140356d84651126368fa16a77f4
Your Name <you@example.com>
2017-10-04 14:17:37 +0300

    good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-<<<<<<< HEAD
 
   def new
       @user = User.new
@@ -23,9 +22,4 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
       end
-=======
-  def show
-   @user = User.find(params[:id])
- end
->>>>>>> sign-up
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'users/new'
-resources :users
- match '/signup',  to: 'users#new',            via: 'get'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'static_pages/index'
+
+get 'static_pages/index'
+  #get "static_pages/home"
   get "static_pages/home"
-  get 'static_pages/help'
-  root 'static_pages#home'
-  get 'static_pages/about'
-get 'static_pages/contact'
-root  'static_pages#home'
-match '/help',    to: 'static_pages#help',    via: 'get'
-match '/about',   to: 'static_pages#about',   via: 'get'
-match '/contact', to: 'static_pages#contact', via: 'get'
-=======
-  resources :users
-  root  'static_pages#home'
+   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
-  get 'static_pages/index'
-  get "static_pages/home"
   match '/help',    to: 'static_pages#help',    via: 'get'
-    match '/about',   to: 'static_pages#about',   via: 'get'
-    match '/contact', to: 'static_pages#contact', via: 'get'
->>>>>>> sign-up
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  get 'static_pages/help'
+  get 'static_pages/about'
+    get 'static_pages/contact'
 end
