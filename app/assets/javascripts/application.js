6f88ff08cdac1162ee7fb46ecebca12f8689f2e5
Your Name <you@example.com>
2017-10-06 16:17:08 +0300

    qsq

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require turbolinks
-//= require bootstrap
+//= require_tree
+
 //= require jquery
 //= require jquery_ujs
-
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
