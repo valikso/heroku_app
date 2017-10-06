502a52358d4e241ba4a859134ce0ad8c43cd0e29
Your Name <you@example.com>
2017-10-06 15:32:37 +0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-#  attr_accessor :password, :password_confirmation
- has_many :microposts
+  has_many :microposts, dependent: :destroy
+    has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+    has_many :followed_users, through: :relationships, source: :followed
+    has_many :reverse_relationships, foreign_key: "followed_id",
+                                       class_name:  "Relationship",
+                                       dependent:   :destroy
+      has_many :followers, through: :reverse_relationships, source: :follower
   before_save { self.email = email.downcase }
    before_create :create_remember_token
     before_save { self.email = email.downcase }
